abc273427cf0f4915db983cee5c06834732d1e35
burchettcaleb <burchettcaleb@gmail.com>
2021-12-02 00:27:33 -0500

    Day 2 done

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,17 +1,55 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.map {it.toInt()}.windowed(2).count { it[1] > it[0] }
+        var depth = 0
+        var horizontalPos = 0
+
+        input.forEach {
+            val split = it.split(" ")
+            val word = split[0]
+            val num = split[1].toInt()
+            when (word){
+                "forward" -> horizontalPos += num
+                "down" -> depth += num
+                "up" -> depth -= num
+                else -> {
+                    println("uh oh")
+                }
+            }
+        }
+
+        return depth * horizontalPos
     }
 
     fun part2(input: List<String>): Int {
-        return input.map {it.toInt()}.windowed(4).count { (it[1] + it[2] + it[3]) > (it[0] + it[1] + it[2]) }
+        var depth = 0
+        var horizontalPos = 0
+        var aim = 0
+
+        input.forEach {
+            val split = it.split(" ")
+            val word = split[0]
+            val num = split[1].toInt()
+            when (word){
+                "forward" -> {
+                    horizontalPos += num
+                    depth += aim * num
+                }
+                "down" -> aim += num
+                "up" -> aim -= num
+                else -> {
+                    println("uh oh")
+                }
+            }
+        }
+
+        return depth * horizontalPos
     }
 
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 7)
-    check(part2(testInput) == 5)
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 150)
+    check(part2(testInput) == 900)
 
-    val input = readInput("Day01")
+    val input = readInput("Day02")
     println(part1(input))
     println(part2(input))
 }
